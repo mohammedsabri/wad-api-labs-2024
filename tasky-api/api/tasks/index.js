55f49d6903a6276b10eaafd3dfc0d6d1edd60ca3
mohammed sabri <mohammedsabri228@gmail.com>
2024-12-14 23:32:02 +0000

    Mongo lab Users Router

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -6,10 +6,10 @@ import asyncHandler from 'express-async-handler';
 const router = express.Router(); // eslint-disable-line
 
 // Get all tasks
-router.post('/', asyncHandler(async (req, res) => {
-    const task = await Task(req.body).save();
-    res.status(201).json(task);
-}));
+router.get('/', async (req, res) => {
+    const tasks = await Task.find().populate('userId', 'username');
+    res.status(200).json(tasks);
+});
 
 // create a task
 router.post('/', async (req, res) => {
