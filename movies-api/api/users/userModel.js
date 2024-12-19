2d1eddacdd51f980cb1159f0a610d162cae7a5d5
mohammed sabri <mohammedsabri228@gmail.com>
2024-12-19 11:22:51 +0000

    Initialise Dev Env - Data

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -14,30 +13,5 @@ const passwordValidator = (password) => {
   return passwordRegex.test(password);
 }
 UserSchema.path("password").validate(passwordValidator);
-
-UserSchema.methods.comparePassword = async function (passw) { 
-  return await bcrypt.compare(passw, this.password); 
-}
-
-UserSchema.statics.findByUserName = function (username) {
-  return this.findOne({ username: username });
-};
-
-UserSchema.pre('save', async function(next) {
-  const saltRounds = 10; // You can adjust the number of salt rounds
-  //const user = this;
-  if (this.isModified('password') || this.isNew) {
-    try {
-      const hash = await bcrypt.hash(this.password, saltRounds);
-      this.password = hash;
-      next();
-  } catch (error) {
-     next(error);
-  }
-
-  } else {
-      next();
-  }
-});
 
 export default mongoose.model('User', UserSchema);
--- movies-api/index.js
+++ movies-api/index.js
@@ -4,8 +4,6 @@ import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
-import moviesRouter from './api/movies';   //import movies router
-import authenticate from './authenticate';
 
 dotenv.config();
 
@@ -15,7 +13,6 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
-app.use('/api/movies', authenticate, moviesRouter);
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
